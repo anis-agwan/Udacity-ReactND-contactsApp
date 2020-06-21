7fa6b6194c53b22f870e34a284d1a63b8dcf0e7b
anis_agwan <anishagwan@gmail.com>
2020-06-21 14:10:05 +0530

    Functional Components without state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "micheal",
+        "name": "Michael Isgrigg",
+        "handle": "michael_isgrigg",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "ryan",
+        "name": "Ryan Kalehoff",
+        "handle": "ryankalehoff",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+  }
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
